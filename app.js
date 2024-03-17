81c7215c187f6adbb711cc873df7f196c9eb08a1
Thejusl999 <tj.conquerer@gmail.com>
2024-03-17 09:42:57 +0530

    Task#2 - Intro to Middlewares

--- app.js
+++ app.js
@@ -1,20 +1,36 @@
-const http = require('http');
-const express=require('express');
-const app=express();
-app.use((req,res,next)=>{
-    console.log('In the middleware!');
-    next();
-});
-app.use((req,res,next)=>{
-    console.log('In another middleware!');
-    res.send('<h1>Hello from Express!</h1>');
-    // res.send('<h2> hello to node js </h2>')
-    // res.send( { key1: value })
-    // res.send( { key1: 'value' })
-});
-// 1. Using createServer first and then doing listen separately
-/* const server=http.createServer(app);
-server.listen(3000); */
+const http = require("http");
+const bodyParser = require("body-parser");
+const express = require("express");
+const app = express();
 
-// 2. Using app.listen-creates the server and listens too in a single line of code
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/", (req, res, next) => {
+  // console.log('This always runs!');
+  next();
+});
+
+app.use("/add-product", (req, res, next) => {
+  // console.log('In another middleware!');
+  res.send(
+    `<form action="/product" method="POST">
+        <input type="text" name="title" placeholder="Enter Product Title"/><br></br>
+        <input type="text" name="size" placeholder="Enter Product Size"/><br></br>
+        <button type="submit">
+            Add Product
+        </button>
+    </form>`
+  );
+});
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  // console.log('In another middleware!');
+  res.send("<h1>Hello from Express!</h1>");
+});
+
 app.listen(3000);
