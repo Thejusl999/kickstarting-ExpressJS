68210c4cf1306824cdaf90a696e8ba9b08c4dc44
Thejusl999 <tj.conquerer@gmail.com>
2024-03-19 17:39:24 +0530

    Task#7 - What the hell are Models

---
+++ models/product.js
@@ -0,0 +1,65 @@
+// const products=[];           //this doesn't store old product when server restarts
+const fs = require("fs");       //to store old products
+const path = require("path");
+const p = path.join(
+    path.dirname(process.mainModule.filename),
+    "data",
+    "products.json"
+);
+
+const getProductsFromFile=(cb)=>{
+    fs.readFile(p,(err,fileContent)=>{
+        if(err){
+            cb([]);
+        }else{
+            cb(JSON.parse(fileContent));
+        }
+    });
+}
+
+module.exports = class Product {
+  constructor(title) {
+    this.title = title;
+  }
+  save() {
+    // products.push(this);
+    getProductsFromFile(products=>{
+        products.push(this);
+        fs.writeFile(p,JSON.stringify(products),err=>{
+            console.log(err);
+        });
+    })
+    // Moved into function(for optimisation)
+    /* const p = path.join(
+        path.dirname(process.mainModule.filename),
+        "data",
+        "products.json"
+    );
+    fs.readFile(p,(err,fileContent)=>{
+        let products=[];
+        if(!err){
+            products=JSON.parse(fileContent);
+        }
+        products.push(this);
+        fs.writeFile(p,JSON.stringify(products),err=>{
+            console.log(err);
+        });
+    }); */
+  }
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+    // Moved into function(for optimisation)
+    /* const p = path.join(
+        path.dirname(process.mainModule.filename),
+        "data",
+        "products.json"
+    );
+    fs.readFile(p,(err,fileContent)=>{
+        if(err){
+            cb([]);
+        }else{
+            cb(JSON.parse(fileContent));
+        }
+    }); */
+  }
+};
